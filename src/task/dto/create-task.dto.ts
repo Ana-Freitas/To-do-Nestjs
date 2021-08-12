2a437a0e40fc9eaea444536fdf3a01aa96ae2675
Ana-Freitas <ana_paula.10@hotmail.com>
2021-08-12 17:06:43 -0300

    Validation: class validator

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,8 +1,21 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTaskDto {
-    readonly _id: number;
+
+    @IsNotEmpty()
+    readonly id: number;
+
+    @IsString()
+    @IsNotEmpty()
     readonly title: string;
+
+    @IsString()
     readonly description: string;
+
     readonly createDate: Date;
+
     readonly finishDate: Date;
+
+    @IsBoolean()
     readonly isDone: boolean;
-}
+} 
